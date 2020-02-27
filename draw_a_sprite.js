ecc972d2496b05f6b92ff32b2a4e4a790ee0fd23
Rutger <rutgerjmckenna@gmail.com>
2020-02-27 18:26:55 -0500

    draw sprite

---
+++ draw_a_sprite.js
@@ -0,0 +1,22 @@
+function preload() {
+    this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png')
+  
+  }
+  
+  function create() {
+    this.add.sprite(40, 80, 'codey');
+  
+  }
+  
+  const config = {
+      type: Phaser.AUTO,
+      width: 450,
+      height: 600,
+      backgroundColor: "#5f2a55",
+      scene: {
+      create,
+      preload
+      }
+  }
+  
+  const game = new Phaser.Game(config)
