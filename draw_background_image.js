38f3d49079a2e7ba62b3d34e72295e4c36c5857c
Rutger <rutgerjmckenna@gmail.com>
2020-02-27 18:28:10 -0500

    draw background

---
+++ draw_background_image.js
@@ -0,0 +1,21 @@
+function preload() {
+    this.load.image('sky', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/sky.jpg')
+  
+  }
+  
+  function create() {
+    this.add.image(100, 100, 'sky')
+  }
+  
+  const config = {
+      type: Phaser.AUTO,
+      width: 450,
+      height: 600,
+      backgroundColor: "#5f2a55",
+      scene: {
+      create,
+      preload
+      }
+  }
+  
+  const game = new Phaser.Game(config)
